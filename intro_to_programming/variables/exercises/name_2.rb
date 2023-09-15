f30ffb1f33316c369dc1146d0393aa45c9809de1
Timothy-James-2 <Timothy.James_8967542@proton.me>
2023-09-15 11:57:52 -0700

    exercise updates

---
+++ intro_to_programming/variables/exercises/name_2.rb
@@ -0,0 +1,8 @@
+# name_2.rb
+
+puts "What is your name?"
+name = gets.chomp
+puts "Hello " + name
+10.times do 
+  puts name 
+end
---
+++ intro_to_programming/variables/exercises/name_3.rb
@@ -0,0 +1,7 @@
+# name_3.rb continued again
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you. What is your last name?"
+last_name = gets.chomp
+puts "Excellent, your full name is " + first_name + " " + last_name
