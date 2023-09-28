c9ec623acf4cac7f43e91333f5c74384a0d3cd58
Timothy-James-2 <Timothy.James_8967542@proton.me>
2023-09-28 12:57:59 -0700

    completed flow control and added loops_iterators

---
+++ intro_to_programming/flow_control/exercises/Snippet_3.rb
@@ -0,0 +1,25 @@
+# Snippet 3
+# y = 9
+# x = 10
+# if (x + 1) <= (y)
+#   puts "Alright."
+# elsif (x + 1) >= (y)
+#   puts "Alright now!"
+# elsif (y + 1) == x
+#   puts "ALRIGHT NOW!"
+# else
+#   puts "Alrighty!"
+# end
+
+y = 10
+x = 11
+
+if (x + 1) <= (y)
+  puts "OK"
+elsif (x + 1) >= (y)
+  puts "OK now!"
+elsif (y + 1) == (x)
+  puts "OK NOW!"
+else
+  puts "ok, whateva"
+end
---
+++ intro_to_programming/flow_control/exercises/caps_method.rb
@@ -0,0 +1,13 @@
+
+# caps_method.rb
+
+def caps(string)
+  if string.length > 10
+    string.upcase
+  else
+    string
+  end
+end
+
+puts caps("Joe Smith")
+puts caps("Joe Robertson")
